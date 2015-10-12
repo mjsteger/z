5da34073bbdbcf99b553760117256de85a9bb549
Michael Steger <mjsteger1@gmail.com>
2015-10-12 09:47:52 -0700

    Adding support for zsh completions

--- z.sh
+++ z.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
 Z_DIRECTORY=$HOME/.z
-complete -C $Z_DIRECTORY/z_sourcing.sh z
+if [ -n "$ZSH_VERSION" ]; then
+    autoload -U _z
+    compdef _z z
+elif [ -n "$BASH_VERSION" ]; then
+    complete -C $Z_DIRECTORY/z_sourcing.sh z
+fi
 
 function z {
     ruby $Z_DIRECTORY/z.rb $@ > /tmp/z.output
